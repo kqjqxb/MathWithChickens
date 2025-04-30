82b42f98b44f21c9c96c1dc2800636a57dfea7c9
kqjqxb <work.maksym.lomakin@gmail.com>
2025-04-30 15:55:31 +0300

    completed onboardingScreen

--- src/components/chickenOnboardingImagesData.js
+++ src/components/chickenOnboardingImagesData.js
@@ -1,26 +1,20 @@
 export default [
   {
     id: 1,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken1.png')
+    itemImage: require('../assets/images/mathOnbImage1.png'),
+    mathMainText: 'Learn Math with Chickens!',
+    mathSecondaryText: 'Help our little chicken learn how to add! Solve fun quizzes, unlock number eggs, and collect them in the mini-game. \nLearning math has never been this fun!',
   },
   {
     id: 2,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken2.png')
+    itemImage: require('../assets/images/mathOnbImage2.png'),
+    mathMainText: 'Solve Addition Problems',
+    mathSecondaryText: 'Each level is a short quiz with simple math problems. \nAnswer correctly to unlock a new number and earn an egg reward! \nThe more you learn — the more exciting it gets.',
   },
   {
     id: 3,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken3.png')
-  },
-  {
-    id: 4,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken4.png')
-  },
-  {
-    id: 5,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken5.png')
-  },
-  {
-    id: 6,
-    itemImage: require('../assets/images/chickenOnboardingImages/chicken6.png')
+    itemImage: require('../assets/images/mathOnbImage3.png'),
+    mathMainText: 'Play the Egg Game!',
+    mathSecondaryText: 'Play a fun mini-game: move your chicken, catch the right eggs, and avoid anvils! \nWith each unlocked number, the game gets trickier. \nReach the target sum — but don’t go over it!',
   },
 ]
--- src/screens/ChickenOnboardingRunScreen.js
+++ src/screens/ChickenOnboardingRunScreen.js
@@ -5,11 +5,14 @@ import { useNavigation } from '@react-navigation/native';
 
 const fontKronaOneRegular = 'KronaOne-Regular';
 
+const fontRammetoOneRegular = 'RammettoOne-Regular';
+const fontRanchersRegular = 'Ranchers-Regular';
+
 const ChickenOnboardingRunScreen = () => {
   const [dimensions, setDimensions] = useState(Dimensions.get('window'));
-  const [currentIndexOfChickenSlide, setCurrentIndexOfChickenSlide] = useState(0);
-  const refOfChicken = useRef(null);
-  const ChickenRefHorizontalScrollX = useRef(new Animated.Value(0)).current;
+  const [thisNathIndexSlide, setThisNathIndexSlide] = useState(0);
+  const mathRefSlides = useRef(null);
+  const mathHorizontallScrollRef = useRef(new Animated.Value(0)).current;
   const navigation = useNavigation();
 
   useEffect(() => {
@@ -22,45 +25,71 @@ const ChickenOnboardingRunScreen = () => {
     };
   }, []);
 
-  const viewableItemsChanged = useRef(({ viewableItems }) => {
-    if (viewableItems && viewableItems.length > 0) {
-      setCurrentIndexOfChickenSlide(viewableItems[0].index);
+  const viewableItemsChanged = useRef(({ viewableMathItems }) => {
+    if (viewableMathItems && viewableMathItems.length > 0) {
+      setThisNathIndexSlide(viewableMathItems[0].index);
     }
   }).current;
 
   const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current;
 
-  const scrollNextChickenSlide = () => {
-    if (currentIndexOfChickenSlide >= chickenOnboardingImagesData.length - 1) {
+  const handleNextMathSlide = () => {
+    if (thisNathIndexSlide >= chickenOnboardingImagesData.length - 1) {
       navigation.replace('ChickenRunHomeScreen');
     } else {
-      refOfChicken.current.scrollToIndex({ index: currentIndexOfChickenSlide + 1 });
+      mathRefSlides.current.scrollToIndex({ index: thisNathIndexSlide + 1 });
     }
   };
 
-  const renderChickenItem = ({ item }) => (
+  const mathItemRender = ({ item }) => (
     <View style={{
-      justifyContent: 'space-between',
-      alignItems: 'center',
       flex: 1,
-      height: dimensions.height,
+      alignItems: 'center',
       width: dimensions.width,
+      justifyContent: 'space-between',
+      height: dimensions.height,
     }}>
       <View style={{
-        alignSelf: 'flex-end',
-        alignItems: 'center',
+        alignSelf: 'center',
         width: dimensions.width,
+        alignItems: 'center',
       }}>
         <Image
           source={item.itemImage}
           style={{
-            height: dimensions.height * 0.65,
             alignSelf: 'center',
-            marginTop: dimensions.height * 0.1,
-            width: dimensions.width * 0.9,
+            marginTop: -dimensions.height * 0.05,
+            width: dimensions.width * 0.8,
+            height: dimensions.height * 0.5,
           }}
           resizeMode="contain"
         />
+
+        <Text
+          style={{
+            textAlign: 'center',
+            marginTop: -dimensions.height * 0.05,
+            fontFamily: fontRammetoOneRegular,
+            paddingHorizontal: dimensions.width * 0.05,
+            color: 'white',
+            fontWeight: 700,
+            fontSize: dimensions.width * 0.06,
+          }}>
+          {item.mathMainText}
+        </Text>
+
+        <Text
+          style={{
+            textAlign: 'center',
+            color: 'white',
+            fontFamily: fontRanchersRegular,
+            fontWeight: 700,
+            fontSize: dimensions.width * 0.05,
+            paddingHorizontal: dimensions.width * 0.05,
+            marginTop: dimensions.height * 0.02,
+          }}>
+          {item.mathSecondaryText}
+        </Text>
       </View>
     </View>
   );
@@ -71,28 +100,23 @@ const ChickenOnboardingRunScreen = () => {
         alignItems: 'center',
         flex: 1,
         justifyContent: 'space-between',
+        backgroundColor: '#86CBDD',
       }}
     >
-      {/* <LinearGradient
-        style={{ position: 'absolute', left: 0, right: 0, top: 0, bottom: 0 }}
-        colors={['#F88700', '#FE1B2F']}
-        start={{ x: 1, y: 1 }}
-        end={{ x: 0, y: 0 }}
-      /> */}
       <View style={{ display: 'flex' }}>
         <FlatList
+          bounces={false}
           horizontal
           scrollEventThrottle={32}
+          ref={mathRefSlides}
+          renderItem={mathItemRender}
           showsHorizontalScrollIndicator={false}
-          ref={refOfChicken}
           onViewableItemsChanged={viewableItemsChanged}
-          renderItem={renderChickenItem}
-          bounces={false}
           keyExtractor={(item) => item.id.toString()}
           data={chickenOnboardingImagesData}
           pagingEnabled
           viewabilityConfig={viewConfig}
-          onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: ChickenRefHorizontalScrollX } } }], {
+          onScroll={Animated.event([{ nativeEvent: { contentOffset: { x: mathHorizontallScrollRef } } }], {
             useNativeDriver: false,
           })}
         />
@@ -100,33 +124,31 @@ const ChickenOnboardingRunScreen = () => {
 
       <TouchableOpacity
         onPress={() => {
-          scrollNextChickenSlide();
+          handleNextMathSlide();
         }}
         style={{
           alignSelf: 'center',
           alignItems: 'center',
-          borderWidth: dimensions.width * 0.003,
+          borderColor: '#000',
+          borderRadius: dimensions.width * 0.028,
           justifyContent: 'center',
-          backgroundColor: '#fff',
-          borderRadius: dimensions.width * 0.0616161,
-          height: dimensions.height * 0.08,
-          width: dimensions.width * 0.8818,
+          borderWidth: dimensions.width * 0.0028,
+          height: dimensions.height * 0.078,
+          backgroundColor: '#FFE066',
+          width: dimensions.width * 0.7,
           position: 'absolute',
           bottom: dimensions.height * 0.05,
-          borderColor: 'black',
-          marginTop: dimensions.height * 0.03,
         }}
       >
         <Text
           style={{
-            textAlign: 'center',
-            color: 'black',
-            fontFamily: fontKronaOneRegular,
+            color: '#5C4033',
+            fontFamily: fontRanchersRegular,
             fontWeight: 700,
-            paddingHorizontal: dimensions.width * 0.05,
-            fontSize: dimensions.width * 0.06,
+            fontSize: dimensions.width * 0.08,
+            textAlign: 'center',
           }}>
-          {currentIndexOfChickenSlide >= chickenOnboardingImagesData.length - 1 ? 'Start' : 'Next'}
+          {thisNathIndexSlide >= chickenOnboardingImagesData.length - 1 ? 'Start Learning' : 'Next'}
         </Text>
       </TouchableOpacity>
     </SafeAreaView>
